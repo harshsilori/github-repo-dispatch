7c6519ce525349642bb91b70bfb5bbbfaa898174
harsh silori <harshsilori7@gmail.com>
2020-11-05 12:04:46 +0530

    added node modules

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { getInput, setFailed } from '@actions/core';
-import { context } from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
-  const clientPayload = getInput('client-payload');
+  const clientPayload = core.getInput('client-payload');
   console.log(`payload name: ${clientPayload}`);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
-  setFailed(error.message);
+  core.setFailed(error.message);
 }
